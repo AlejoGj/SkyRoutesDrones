992e1b377f4c396e36ac32a3b8d84b07f83794d6
Juan Manuel Pava Higuita <juanmanuel.pavah@gmail.com>
2026-09-12 11:15:58 -0500

    feat: Se indica la creación de nuevas clases de excepcion necesarias para la realizacion de HU-03

--- src/excepciones/errores.py
+++ src/excepciones/errores.py
@@ -15,3 +15,32 @@ class EstadoMotorInvalidoError(Exception):
 
 class CoordenadaInvalidaError(Exception):
     pass
+
+
+
+# Las Siguientes excepciones son necesarias para poder realizar HU-03, implementó Juan Manuel Pava Higuita (HU-03):
+class DronError(Exception):
+    """ Excepcion base para errores relacionados con drones. """
+    def __init__(self, id_dron: str, message: str) -> None:
+        super().__init__(f"[Dron {id_dron}]: {message}")
+        self.id_dron = id_dron
+        self.message = message
+
+
+class ActualizarEstadoDronError(DronError):
+    """ Excepción para errores al actualizar el estado de un dron. """
+    def __init__(self, id_dron: str, parametro: str) -> None:
+        message = f"{parametro} no es un valor válido para actualizar el estado del dron."
+        super().__init__(id_dron, message)
+        self.parametro = parametro
+
+class EstadoDronDuplicadoError(DronError):
+    """Excepción cuando se intenta cambiar el dron a un estado en el que ya está."""
+    def __init__(self, id_dron: str, estado_actual: bool) -> None:
+        estado_str = "Disponible" if estado_actual else "No Disponible"
+        message = f"El dron ya se encuentra en el estado: {estado_str}."
+        super().__init__(id_dron, message)
+        self.estado_actual = estado_actual
+
+
+    
