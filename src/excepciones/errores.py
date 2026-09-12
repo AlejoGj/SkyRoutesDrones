93cab61920c8540f662ee96a16b1c35f783cdb43
AlejoGj <alejandrogj1209@gmail.com>
2026-09-12 16:58:04 -0500

    feat: creacion de las exceptions para telemetria

--- src/excepciones/errores.py
+++ src/excepciones/errores.py
@@ -2,16 +2,24 @@
 # Responsable: Alejandro García Jiménez
 
 class BateriaInvalidaError(Exception):
-    pass
+    def __init__(self, bateria_invalida: int, message: str = "Batería inválida.") -> None:
+        super().__init__(message)
+        self.bateria_invalida = bateria_invalida
 
 
 class AltitudInvalidaError(Exception):
-    pass
+    def __init__(self, altitud_invalida: float, message: str = "Altitud inválida.") -> None:
+        super().__init__(message)
+        self.altitud_invalida = altitud_invalida
 
 
 class EstadoMotorInvalidoError(Exception):
-    pass
+    def __init__(self, estado_motor_invalido: str, message: str = "Estado del motor inválido.") -> None:    
+        super().__init__(message)
+        self.estado_motor_invalido = estado_motor_invalido
 
 
 class CoordenadaInvalidaError(Exception):
-    pass
+    def __init__(self, coordenada_invalida: tuple, message: str = "Coordenada inválida.") -> None:
+        super().__init__(message)
+        self.coordenada_invalida = coordenada_invalida
