6c3d79843ae492cfa37c39e98dad9b5f52caab03
Juan Manuel Pava Higuita <juanmanuel.pavah@gmail.com>
2026-09-12 11:14:47 -0500

    feat: Se inicializa la clase Dron con sus atributos __init__ y se implementa el metodo actualizar_estado_disponibilidad()

--- src/flota/dron.py
+++ src/flota/dron.py
@@ -4,11 +4,43 @@
 from src.geodesia.calculador import CalculadorGeodesico
 from src.telemetria.telemetria import TelemetriaDrone
 
+# Las siguientes importaciones de excepciones son necesarias para poder realizar HU-03
+from src.excepciones.errores import (
+    ActualizarEstadoDronError,
+    EstadoDronDuplicadoError,
+)
 
 class Dron:
-    def __init__(self, id_dron, modelo_aeronave, categoria, velocidad_promedio_kmh, telemetria=None):
+    def __init__(self, id_dron: str, modelo_aeronave: str, categoria: str, velocidad_promedio_kmh: float, kilometraje_total_km: float, disponible: bool = True)-> None:
+        self.id_dron = id_dron
+        self.modelo_aeronave = modelo_aeronave
+        self.categoria = categoria
+        self.velocidad_promedio_kmh = velocidad_promedio_kmh
+        self.kilometraje_total_km = kilometraje_total_km
+        self.disponible = disponible
+        self.telemetria = None  # Inicialmente no hay telemetría asignada
+    
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
         pass
 
+    def actualizar_estado_disponibilidad(self, disponible: bool) -> None:
+        # Agrego Funcionalidad. HU-03, UML, Responsabilidades no coinciden. Se agrega el atributo disponible (como se dice en el UML) para poder actualizar el estado de disponibilidad del dron.
+        
+        if not isinstance(disponible, bool):
+            # Lanzo una excepción si el valor proporcionado no es un booleano
+            raise ActualizarEstadoDronError(self.id_dron, str(disponible))
+        
+        if disponible == self.disponible:
+            # Lanzo una excepción si el dron ya está en el estado deseado
+            raise EstadoDronDuplicadoError(self.id_dron, self.disponible)
+        
+        # Si pasa las validaciones, actualizo el estado de disponibilidad del dron
+        self.disponible = disponible
+        
+        
     def asignar_telemetria(self, nueva_telemetria):
         pass
 
@@ -24,8 +56,4 @@ class Dron:
     def aterrizar_en_base(self):
         pass
 
-    def __str__(self):
-        pass
-
-    def __repr__(self):
-        pass
+    
